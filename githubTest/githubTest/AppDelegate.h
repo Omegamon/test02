ebb66077ac65e9f77f3040e7b551f3275bde0114
didu <1255579230@qq.com>
2016-07-03 20:23:36 +0800

    初始化

---
+++ githubTest/githubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubTest
+//
+//  Created by Mas on 16/7/3.
+//  Copyright © 2016年 Mas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubTest/githubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubTest
+//
+//  Created by Mas on 16/7/3.
+//  Copyright © 2016年 Mas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
